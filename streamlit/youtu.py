a9c26dd87de6eef79b6bfeca9178b838d6f839db
pincode840 <dlrmflf@gmail.com>
2024-04-19 13:16:59 +0900

    change

--- streamlit/youtu.py
+++ streamlit/youtu.py
@@ -5,6 +5,8 @@ yt = YouTube('https://youtu.be/Vy1JwiXHwI4?si=eZ1dsNDgphBMR0TR')
 print(yt.title)
 yt = yt.streams.get_highest_resolution().download()
 
-filePath = yt.replace('mp4', 'mp3')
-os.rename(yt, filePath)
-print(f'name = {filePath}')
+
+def change_mp():
+    filePath = yt.replace('mp4', 'mp3')
+    os.rename(yt, filePath)
+    print(f'name = {filePath}')
